c9d0d24c50f4e680a287220d746aed556c85589e
netanlb <netanlb@gmail.com>
2023-07-05 14:29:33 +0300

    inject logEntryRepository to LogController

--- src/main/java/com/example/devopsfinal/controllers/LogController.java
+++ src/main/java/com/example/devopsfinal/controllers/LogController.java
@@ -1,4 +1,5 @@
 package com.example.devopsfinal.controllers;
+import com.example.devopsfinal.repositories.LogEntryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,11 @@ import java.util.List;
 
 @RestController
 public class LogController {
+    private final LogEntryRepository logEntryRepository;
+
+    public LogController(LogEntryRepository logEntryRepository) {
+        this.logEntryRepository = logEntryRepository;
+    }
     @GetMapping("/logs")
     public String showLogs() {
         // Add logic here to fetch and return logs
