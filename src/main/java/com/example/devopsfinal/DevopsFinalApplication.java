9b4b7dde323fe2eae052b788a9892db55e2ed896
netanlb <netanlb@gmail.com>
2023-07-05 14:26:02 +0300

    added seed class that logs an entry at /seed with the timestamp at the start of the application

--- src/main/java/com/example/devopsfinal/DevopsFinalApplication.java
+++ src/main/java/com/example/devopsfinal/DevopsFinalApplication.java
@@ -1,7 +1,14 @@
 package com.example.devopsfinal;
 
+import com.example.devopsfinal.models.LogEntry;
+import com.example.devopsfinal.repositories.LogEntryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import com.example.devopsfinal.models.LogEntry;
 
 @SpringBootApplication
 public class DevopsFinalApplication {
@@ -10,4 +17,11 @@ public class DevopsFinalApplication {
 		SpringApplication.run(DevopsFinalApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(LogEntryRepository repository) {
+		return (args) -> {
+			// Save a new log entry at application startup
+			repository.save(new LogEntry("/seed", new Date()));
+		};
+	}
 }
--- src/main/java/com/example/devopsfinal/models/LogEntry.java
+++ src/main/java/com/example/devopsfinal/models/LogEntry.java
@@ -7,12 +7,32 @@ import java.util.Date;
 public class LogEntry {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private Date timestamp;
     private String endpoint;
 
-    protected LogEntry() {
+    protected LogEntry() {}
+
+    public LogEntry(String endpoint, Date timestamp) {
+        this.endpoint = endpoint;
+        this.timestamp = timestamp;
+    }
+    // getters and setters
+
+    public String getEndpoint() {
+        return endpoint;
     }
 
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
