45523a00c4641496392ee67eb0c687b95459d801
netanlb <netanlb@gmail.com>
2023-07-05 14:06:21 +0300

    init project with depenedcies using Spring Initializr

---
+++ src/main/java/com/example/devopsfinal/DevopsFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsfinal/DevopsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
